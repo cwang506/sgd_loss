21fd6b857c484bc07703115dfc1eae4afc37cf70
cwang506 <cwang506@mit.edu>
2020-11-24 10:54:06 -0500

    added preliminary model/data stuff

---
+++ python/model.py
@@ -0,0 +1,7 @@
+from torch.nn import Softplus #smooth relu
+import torch.nn as nn
+import torch.nn.functional as F
+
+def Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
